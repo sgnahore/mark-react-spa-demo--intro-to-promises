26afa815f6b99c0d85e922c5d54653a337aeba3a
Richard Ng <richardcrng@gmail.com>
2021-02-20 13:33:00 +0000

    Create comparison of promises demo

--- src/0-promise-demo.ts
+++ src/0-promise-demo.ts
@@ -1,11 +1,11 @@
 import sleep from "./helpers/sleep";
 
 function printWithDelay(message: string) {
-  console.log("Before executing the promise");
+  console.log("Before creating the promise");
   sleep(2000).then(() =>
-    console.log("After promise has been 'resolved'", message)
+    console.log("Promise has now been 'resolved':", message)
   );
-  console.log("After executing the promise");
+  console.log("After creating the promise");
 }
 
 printWithDelay("Hello world!");

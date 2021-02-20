18cf1711f6f6b0a555ab2c7587e8027637bb75ef
Richard Ng <richardcrng@gmail.com>
2021-02-20 13:36:21 +0000

    Add clearer line spacing

--- src/1-promise-resolving.ts
+++ src/1-promise-resolving.ts
@@ -2,13 +2,17 @@ import sleep from "./helpers/sleep";
 
 function printWithCompetingDelays(firstMessage: string, secondMessage: string) {
   console.log("Before creating promise one");
+
   sleep(2000).then(() =>
     console.log("After FIRST promise has been 'resolved':", firstMessage)
   );
+
   console.log("Before creating promise two (but after creating promise two)");
+
   sleep(1000).then(() =>
     console.log("After SECOND promise has been 'resolved':", secondMessage)
   );
+
   console.log("After creating both promises");
 }
 

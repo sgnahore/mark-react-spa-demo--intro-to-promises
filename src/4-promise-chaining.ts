a6684705c57250a6eb5063514471d993eaab816a
Richard Ng <richardcrng@gmail.com>
2021-02-20 15:32:16 +0000

    Simplify promise chaining example

--- src/4-promise-chaining.ts
+++ src/4-promise-chaining.ts
@@ -1,6 +1,6 @@
 import sleep from "./helpers/sleep";
 
-function printWithDelay(message: string) {
+function printWithDelay() {
   console.log("START of function body");
 
   const promiseOne = sleep(2000);
@@ -8,23 +8,23 @@ function printWithDelay(message: string) {
     console.log("resolved first promise")
   );
   const promiseThree = promiseTwo.then(() =>
-    console.log("resolved second promise", message)
+    console.log("resolved second promise")
   );
   promiseThree.then(() => console.log("resolved third promise"));
 
   console.log("END of function body");
 }
 
-// function printWithDelay(message: string) {
+// function printWithDelay() {
 //   console.log("START of function body");
 
 //   const promiseOne = sleep(2000);
 //   promiseOne
 //     .then(() => console.log("resolved first promise"))
-//     .then(() => console.log("resolved second promise", message))
+//     .then(() => console.log("resolved second promise"))
 //     .then(() => console.log("resolved third promise"));
 
 //   console.log("END of function body");
 // }
 
-printWithDelay("hello world!");
+printWithDelay();

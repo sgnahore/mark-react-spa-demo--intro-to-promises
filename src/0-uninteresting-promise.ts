5852b05ac25eeee2f7a6afc20d1fac25e06203af
Richard Ng <richardcrng@gmail.com>
2021-02-20 13:50:41 +0000

    Make examples easier to read

---
+++ src/0-uninteresting-promise.ts
@@ -0,0 +1,21 @@
+import sleep from "./helpers/sleep";
+
+function printStraightforwardly(message: string) {
+  console.log("START of function body");
+
+  console.log(message);
+
+  console.log("END of function body");
+}
+
+function printWithSleep(message: string) {
+  console.log("START of function body");
+
+  sleep(2000);
+  console.log(message);
+
+  console.log("END of function body");
+}
+
+printStraightforwardly("Hello world!");
+// printWithSleep("Hello world!");
